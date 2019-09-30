5aefc3bc08d278b147320ace6e16925716dca76b
Dejan Noveski <dr.mote@gmail.com>
2019-09-30 22:40:15 +0200

    Bump Travis Python version to 3.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
     ],
     url='https://github.com/Virtualstock/forkit-django/',
